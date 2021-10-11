aaaaa56707429ad286f0a4e8b3bc1298c1d1cc66
Sam Low <slowc4g@gmail.com>
2021-10-10 22:02:46 -0700

    Finish Q2 string parsing and calculator

---
+++ calculator.py
@@ -0,0 +1,57 @@
+'''
+Parses a user-given string into a mathematical expression.
+Params:
+    None
+Returns:
+    Nothing
+'''
+
+
+def parse_input():
+    eqn = input('Enter equation: ')
+    nums = []
+    op = ''
+    for t in eqn.split():
+        try:
+            nums.append(float(t))
+        except ValueError:
+            pass
+    for i in range(0, len(eqn)):
+        if not (eqn[i].isalnum() or eqn[i].isspace() or eqn[i] == '.'):
+            op += eqn[i]
+    print(calculator(nums[0], nums[1], op))
+    return
+
+
+'''
+Calculates a mathematical expression and returns its result.
+Params:
+    number1: Any number.
+    number2: Any number.
+    operator: The operation to use on the above numbers.
+Operator:
+    '+': Adds number1 and number2.
+    '-': Subtracts number1 and number2.
+    '*': Multiplies number1 and number2.
+    '/': Divides number1 and number2.
+    '**': Raises number1 to the number2 power.
+    '//': Divides number1 and number2, then takes the floor of the quotient.
+
+Returns:
+    result: The result of the calculation.
+'''
+
+
+def calculator(number1, number2, operator):
+    if operator == '+':
+        return number1 + number2
+    elif operator == '-':
+        return number1 - number2
+    elif operator == '*':
+        return number1 * number2
+    elif operator == '/':
+        return number1 / number2
+    elif operator == '**':
+        return number1 ** number2
+    elif operator == '//':
+        return number1 // number2
